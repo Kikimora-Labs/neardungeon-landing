cf9c66f005f0394d03174fe9b801304a27c13cbc
Vadim B <vadim.bobrovitsky@gmail.com>
2022-02-14 00:22:58 +0300

    mob -v

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,75 +1,5 @@
 addPlayersToHTMLLists()
 
-// elements for parallax
-const header = document.querySelector('.header')
-const contentsSection = document.querySelector('.contents')
-
-const headerPaddingBottom = Number.parseFloat(window.getComputedStyle(header).paddingBottom)
-
-// header cards
-const card1  = document.querySelector('#game-card-1')
-const card2  = document.querySelector('#game-card-2')
-const card3  = document.querySelector('#game-card-3')
-const card4  = document.querySelector('#game-card-4')
-
-window.addEventListener('scroll', e => {
-  window.requestAnimationFrame(all)
-
-  // window.requestAnimationFrame(headerParallax)  
-})
-
-function all() {  
-  headerParallax()
-  // glideElementOnScroll()
-}
-
-function headerParallax() {  
-  const value = window.scrollY
-
-  if (value <= headerPaddingBottom) {
-    header.style.paddingBottom = (headerPaddingBottom - value) + 'px'
-  }
-  
-  card1.style.transform = `translateY(-${(value / 10)}%)`
-  card2.style.transform = `translateY(-${(value / 5)}%)`
-  card3.style.transform = `translateY(-${(value / 20)}%)`
-  card4.style.transform = `translateY(-${(value / 15)}%)`
-}
-
-// move element from side to side on scroll (***for scroll event***)
-function glideElementOnScroll() {  
-  console.log(isScrolledIntoView(contentsSection));
-  if (isScrolledIntoView(contentsSection)) {
-    const value = window.scrollY / 100
-    let step = 100 * value
-    let currX = 0
-
-    currX += step
-
-    contentsSection.style.transform = `translateX(-${currX}%)` 
-  } else {
-    contentsSection.style.transform = `translateX(0)` 
-  } 
-}
-
-function getCoords(elem) {
-  let box = elem.getBoundingClientRect();
-
-  return box
-}
-
-function isScrolledIntoView(el) {
-  // check for 0.1% visible
-  var percentVisible = 0.01;
-  var elemTop = el.getBoundingClientRect().top;
-  var elemBottom = el.getBoundingClientRect().bottom;
-  var elemHeight = el.getBoundingClientRect().height;
-  var overhang = elemHeight * (1 - percentVisible);
-
-  var isVisible = (elemTop >= -overhang) && (elemBottom <= window.innerHeight + overhang);
-  return isVisible;
-}
-
 function addPlayersToHTMLLists() {
   const firstPlayersListEl = document.querySelector('.players-list:first-child')
   const secondPlayersListEl = document.querySelector('.players-list:last-child')
@@ -114,5 +44,78 @@ function addPlayersToHTMLLists() {
         <p class="players-item__joined">Joined today</p>
       </li>
     `
+  }
+}
+
+if (window.matchMedia('(min-width: 900px)')) {
+  // elements for parallax
+  const header = document.querySelector('.header')
+  const contentsList = document.querySelector('.contents__list')
+
+  const headerPaddingBottom = Number.parseFloat(window.getComputedStyle(header).paddingBottom)
+
+  // header cards
+  const card1  = document.querySelector('#game-card-1')
+  const card2  = document.querySelector('#game-card-2')
+  const card3  = document.querySelector('#game-card-3')
+  const card4  = document.querySelector('#game-card-4')
+
+  window.addEventListener('scroll', e => {
+    window.requestAnimationFrame(all)
+  })
+
+  function all() {  
+    headerParallax()
+    // glideElementOnScroll()
+  }
+
+  function headerParallax() {  
+    const value = window.scrollY
+
+    if (value > 2000) {
+      return
+    }
+
+    if (value <= headerPaddingBottom) {
+      header.style.paddingBottom = (headerPaddingBottom - value) + 'px'
+    }
+    
+    card1.style.transform = `translateY(-${(value / 10)}%)`
+    card2.style.transform = `translateY(-${(value / 5)}%)`
+    card3.style.transform = `translateY(-${(value / 20)}%)`
+    card4.style.transform = `translateY(-${(value / 15)}%)`
+  }
+
+  // move element from side to side on scroll (***for scroll event***)
+  function glideElementOnScroll() {
+    if (isScrolledIntoView(contentsList)) {
+      const value = window.scrollY / 100
+      let step = 100 * value
+      let currX = 0
+
+      currX += value
+
+      contentsList.style.transform = `translateX(-${currX}%)` 
+    } else {
+      contentsList.style.transform = `translateX(0)` 
+    } 
+  }
+
+  function getCoords(elem) {
+    let box = elem.getBoundingClientRect();
+
+    return box
+  }
+
+  function isScrolledIntoView(el) {
+    // check for 0.1% visible
+    var percentVisible = 0.01;
+    var elemTop = el.getBoundingClientRect().top;
+    var elemBottom = el.getBoundingClientRect().bottom;
+    var elemHeight = el.getBoundingClientRect().height;
+    var overhang = elemHeight * (1 - percentVisible);
+
+    var isVisible = (elemTop >= -overhang) && (elemBottom <= window.innerHeight + overhang);
+    return isVisible;
   }
 }
