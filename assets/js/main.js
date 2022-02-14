826248e33e08bc97424267f3f4ec2966feb8679b
Vadim B <vadim.bobrovitsky@gmail.com>
2022-02-14 23:35:52 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,121 +1,94 @@
-addPlayersToHTMLLists()
+const firstPlayersListNames  = [
+  'Carstein',
+  'narntt',
+  'vasyan_pro_2010',
+  'Rivka',
+  'Sindirella',
+  'Galina68ru',
+  'Stas_ka',
+  'InKey',
+  'Fergus',
+  'Zaina',
+  'Alcock',
+  'French',
+  'Livingston',
+  'Rahman',
+  'Ellwood',
+  'Lozano',
+  'Gamble',
+]
+const secondPlayersListNames = [
+  'Qurad',
+  'Arthas',
+  'Halabete',
+  'Nanysoan',
+  'Helle',
+  'Prghelib',
+  'Qurad',
+  'Necromancer',
+  'Ammaarah',
+  'Decker',
+  'Crawford',
+  'Ortiz',
+  'Hicks',
+  'Devlin',
+  'Jolene',
+  'Karolina',
+  'Rocky',
+]
 
-function addPlayersToHTMLLists() {
-  const firstPlayersListEl = document.querySelector('.players-list:first-child')
-  const secondPlayersListEl = document.querySelector('.players-list:last-child')
+const firstPlayersListEl  = document.querySelector('.players-list:first-child')
+const secondPlayersListEl = document.querySelector('.players-list:last-child')
 
-  const firstPlayersListNames = [
-    'Carstein',
-    'narntt',
-    'vasyan_pro_2010',
-    'Rivka',
-    'Sindirella',
-    'Galina68ru',
-    'Stas_ka',
-    'InKey'
-  ]
+let firstPlayersListHTML  = ''
+let secondPlayersListHTML = ''
 
-  const secondPlayersListNames = [
-    'Qurad',
-    'Arthas',
-    'Halabete',
-    'Nanysoan',
-    'Helle',
-    'Prghelib',
-    'Qurad',
-    'Necromancer'
-  ]
-
-  let firstPlayersListHTML = ''
-  let secondPlayersListHTML = ''
-
-  for (let i = 0, length = firstPlayersListNames.length; i < length; i++) {
-    firstPlayersListHTML += getLiHTML(firstPlayersListNames[i])
-    secondPlayersListHTML += getLiHTML(secondPlayersListNames[i])
-  }
-
-  firstPlayersListEl.innerHTML = firstPlayersListHTML
-  secondPlayersListEl.innerHTML = secondPlayersListHTML
-
-  function getLiHTML(playerName) {
-    return `
-      <li class="players-item">
-        <p class="players-item__name">${playerName}</p>
-        <p class="players-item__joined">Joined today</p>
-      </li>
-    `
-  }
+for (let i = 0, length = firstPlayersListNames.length; i < length; i++) {
+  firstPlayersListHTML  += getLiHTML(firstPlayersListNames[i])
+  secondPlayersListHTML += getLiHTML(secondPlayersListNames[i])
 }
 
-if (window.matchMedia('(min-width: 900px)')) {
-  // elements for parallax
-  const header = document.querySelector('.header')
-  const contentsList = document.querySelector('.contents__list')
+firstPlayersListEl.innerHTML  = firstPlayersListHTML
+secondPlayersListEl.innerHTML = secondPlayersListHTML
 
-  const headerPaddingBottom = Number.parseFloat(window.getComputedStyle(header).paddingBottom)
+function getLiHTML(playerName) {
+  return `
+    <li class="players-item">
+      <p class="players-item__name">${playerName}</p>
+      <p class="players-item__joined">Joined today</p>
+    </li>
+  `
+}
 
-  // header cards
-  const card1  = document.querySelector('#game-card-1')
-  const card2  = document.querySelector('#game-card-2')
-  const card3  = document.querySelector('#game-card-3')
-  const card4  = document.querySelector('#game-card-4')
+// elements for parallax
+const header = document.querySelector('.header')
+const contentsList = document.querySelector('.contents__list')  
 
-  window.addEventListener('scroll', e => {
-    window.requestAnimationFrame(all)
-  })
+// header cards
+const card1  = document.querySelector('#game-card-1')
+const card2  = document.querySelector('#game-card-2')
+const card3  = document.querySelector('#game-card-3')
+const card4  = document.querySelector('#game-card-4')
 
-  function all() {  
-    headerParallax()
-    // glideElementOnScroll()
+const headerPaddingBottom = Number.parseFloat(window.getComputedStyle(header).paddingBottom)
+
+window.addEventListener('scroll', e => {
+  window.requestAnimationFrame(headerParallax)
+})
+
+function headerParallax() {  
+  const value = window.scrollY
+
+  if (value > 2000) {
+    return
   }
 
-  function headerParallax() {  
-    const value = window.scrollY
-
-    if (value > 2000) {
-      return
-    }
-
-    if (value <= headerPaddingBottom) {
-      header.style.paddingBottom = (headerPaddingBottom - value) + 'px'
-    }
-    
-    card1.style.transform = `translateY(-${(value / 10)}%)`
-    card2.style.transform = `translateY(-${(value / 5)}%)`
-    card3.style.transform = `translateY(-${(value / 20)}%)`
-    card4.style.transform = `translateY(-${(value / 15)}%)`
+  if (value <= headerPaddingBottom) {
+    header.style.paddingBottom = (headerPaddingBottom - value) + 'px'
   }
-
-  // move element from side to side on scroll (***for scroll event***)
-  function glideElementOnScroll() {
-    if (isScrolledIntoView(contentsList)) {
-      const value = window.scrollY / 100
-      let step = 100 * value
-      let currX = 0
-
-      currX += value
-
-      contentsList.style.transform = `translateX(-${currX}%)` 
-    } else {
-      contentsList.style.transform = `translateX(0)` 
-    } 
-  }
-
-  function getCoords(elem) {
-    let box = elem.getBoundingClientRect();
-
-    return box
-  }
-
-  function isScrolledIntoView(el) {
-    // check for 0.1% visible
-    var percentVisible = 0.01;
-    var elemTop = el.getBoundingClientRect().top;
-    var elemBottom = el.getBoundingClientRect().bottom;
-    var elemHeight = el.getBoundingClientRect().height;
-    var overhang = elemHeight * (1 - percentVisible);
-
-    var isVisible = (elemTop >= -overhang) && (elemBottom <= window.innerHeight + overhang);
-    return isVisible;
-  }
+  
+  card1.style.transform = `translateY(-${(value / 10)}%)`
+  card2.style.transform = `translateY(-${(value / 5)}%)`
+  card3.style.transform = `translateY(-${(value / 20)}%)`
+  card4.style.transform = `translateY(-${(value / 15)}%)`
 }
